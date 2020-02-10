96ecf496c975f53608b992343c1e8e914bf625e8
mthang1801 <mthang1801@gmail.com>
2020-02-10 23:32:39 +0700

    version [1.0.0] design authentication form

---
+++ src/public/js/auth.js
@@ -0,0 +1,26 @@
+
+
+
+let showRegisterForm = () => {
+  $(".tab-pane").each((index,element) => {
+    $(element).removeClass("active show");
+  })
+  $(".nav-link").each((index,element) => {
+    $(element).removeClass("active");
+  })
+  
+  $("#btn-register").click();
+}
+
+let showLoginForm = () => {
+  $(".tab-pane").each((index,element) => {
+    $(element).removeClass("active show");
+  })
+  $(".nav-link").each((index,element) => {
+    $(element).removeClass("active");
+  })
+  
+  $("#btn-login").click();
+}
+
+
